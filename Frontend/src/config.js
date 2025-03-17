44fa535b8b602069a1cdd180d22442a6b30c62dc
Aaron Biswas <savybiswas15@gmail.com>
2025-03-17 23:15:17 +0530

    Render compatibility

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.PROD 
-  ? '' // Empty string for same-origin requests in production
+  ? "https://bookstoreapp-jau4.onrender.com" // Empty string for same-origin requests in production
   : 'http://localhost:4001';
 
 export default API_BASE_URL; 
