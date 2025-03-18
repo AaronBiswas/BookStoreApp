09b7085dd7b89a4d3b3d2858cbe5561834845c08
Aaron Biswas <savybiswas15@gmail.com>
2025-03-18 13:58:58 +0530

    bugs fixes

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.PROD 
-  ? import.meta.env.VITE_API_BASE_URL  // Empty string for same-origin requests in production
+  ? "https://bookstoreapp-jau4.onrender.com" // Empty string for same-origin requests in production
   : 'http://localhost:4000';
 
 export default API_BASE_URL; 
